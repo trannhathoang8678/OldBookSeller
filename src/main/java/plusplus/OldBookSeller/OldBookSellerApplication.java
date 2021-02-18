3875e3d6f60f9770b240de7b02ff418832b032a1
raven <trannhathoang8678@gmail.com>
2021-02-18 15:44:13 +0700

    create structure

---
+++ src/main/java/plusplus/OldBookSeller/OldBookSellerApplication.java
@@ -0,0 +1,13 @@
+package plusplus.OldBookSeller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldBookSellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OldBookSellerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/plusplus/OldBookSeller/OldBookSellerApplicationTests.java
@@ -0,0 +1,13 @@
+package plusplus.OldBookSeller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OldBookSellerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
