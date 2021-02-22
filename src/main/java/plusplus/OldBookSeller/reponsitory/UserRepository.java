c007b14fc47940dc302be0fd6b4a0c0bfa512699
raven <trannhathoang8678@gmail.com>
2021-02-22 09:40:25 +0700

    update UserRepo

--- src/main/java/plusplus/OldBookSeller/reponsitory/UserRepository.java
+++ src/main/java/plusplus/OldBookSeller/reponsitory/UserRepository.java
@@ -9,4 +9,5 @@ import java.util.List;
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
     List<User> findAllByEmailAndPassword(String email,String password);
+    boolean existsByEmail(String email);
 }
